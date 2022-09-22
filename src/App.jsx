463694df43655052046f80353900b1e2005c7de1
Dalvin Segura <byjacson18@gmail.com>
2022-09-22 12:21:14 -0400

    Allcountries: i created the searcher functionality and works perfectly

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./app.scss";
 import AllCountries from "./components/allCountries/AllCountries";
 import Navbar from "./components/navbar/Navbar";
-import Searcher from "./components/seacher/Searcher";
 
 function App() {
   return (
     <div>
       <Navbar />
       <main>
-        <Searcher />
         <AllCountries />
       </main>
     </div>
--- src/components/allCountries/AllCountries.jsx
+++ src/components/allCountries/AllCountries.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import "./all-countries.scss";
+import "./searcher.scss";
+// import Searcher from "./../seacher/Searcher";
 
 const AllCountries = () => {
   const [dataCountry, setDataCountry] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const fetchingData = async () => {
@@ -10,41 +13,67 @@ const AllCountries = () => {
       const data = await res.json();
 
       setDataCountry(data);
-      console.log(data);
+      // console.log(data);
     };
     fetchingData();
   }, []);
 
   const PreviewCard = () => {
-    return dataCountry.map((country) => (
-      <div key={country.name.official} className="preview-card">
-        <div className="img-container">
-          <img src={country.flags.png} alt="a" />
-        </div>
+    return dataCountry
+      .filter((val) => {
+        if (searchTerm == "") {
+          return val;
+        } else if (
+          val.name.official.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          return val;
+        }
+      })
+      .map((country) => (
+        <div key={country.name.official} className="preview-card">
+          <div className="img-container">
+            <img src={country.flags.png} alt="a" />
+          </div>
 
-        <div className="preview-text-content">
-          <h3>{country.name.official}</h3>
+          <div className="preview-text-content">
+            <h3>{country.name.official}</h3>
 
-          <h5 className="country-details">
-            Population:{" "}
-            <span className="values">
-              {country.population.toLocaleString()}
-            </span>
-          </h5>
-          <h5 className="country-details">
-            Region: <span className="values">{country.region}</span>
-          </h5>
-          <h5 className="country-details">
-            Capital: <span className="values">{country.capital}</span>
-          </h5>
+            <h5 className="country-details">
+              Population:{" "}
+              <span className="values">
+                {country.population.toLocaleString()}
+              </span>
+            </h5>
+            <h5 className="country-details">
+              Region: <span className="values">{country.region}</span>
+            </h5>
+            <h5 className="country-details">
+              Capital: <span className="values">{country.capital}</span>
+            </h5>
+          </div>
         </div>
-      </div>
-    ));
+      ));
   };
 
   return (
-    <div className="preview-container">
-      <PreviewCard />
+    <div>
+      <div className="searcher">
+        <label>
+          <i className="fa-solid fa-magnifying-glass icon-searcher"></i>
+          <input
+            type="text"
+            className="input-searcher"
+            placeholder="Search for a country..."
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
+          />
+        </label>
+      </div>
+
+      <div className="preview-container">
+        <PreviewCard />
+      </div>
     </div>
   );
 };
